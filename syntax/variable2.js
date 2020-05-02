3804bf771ade4f90a95db104ff510c59b0896b8b
Bomin Kim <bomnie.kim@gmail.com>
2020-05-02 16:03:49 +0900

    variable

---
+++ syntax/variable2.js
@@ -0,0 +1,4 @@
+var name = 'bomin';
+var letter = 'Dear '+name+'Lorem ipsum, dolor sit amet consectetur adipisicing elit. '+name+'Quidem commodi fuga consectetur ea facilis ullam, libero quas culpa minima iste, veniam obcaecati laudantium possimus'+name+' dolores quibusdam quisquam nesciunt ab voluptas.'+name;
+// 변수를 통한 중복제거 , 코드의 가독성
+console.log(letter);
