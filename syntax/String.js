262461c2b3c2da8c72c466761f7fdb86b9231255
Bomin Kim <bomnie.kim@gmail.com>
2020-05-02 16:03:07 +0900

    string

---
+++ syntax/String.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');
+console.log('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem commodi fuga consectetur ea facilis ullam, libero quas culpa minima iste, veniam obcaecati laudantium possimus dolores quibusdam quisquam nesciunt ab voluptas.'.length);
