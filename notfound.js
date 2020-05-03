1d26b4aa614bf2d93e610ce971656687fc009940
Bomin Kim <bomnie.kim@gmail.com>
2020-05-03 20:24:24 +0900

    404 not found 구현

---
+++ notfound.js
@@ -0,0 +1,42 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url"); // 모듈 url
+
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathName = url.parse(_url, true).pathname;
+  var title = queryData.id;
+
+  if (pathName == "/") {
+    fs.readFile(`data/${queryData.id}`, "utf8", function (err, desc) {
+      var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=html">HTML</a></li>
+            <li><a href="/?id=css">CSS</a></li>
+            <li><a href="/?id=javascript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${desc}</p>
+        </body>
+        </html>
+        
+        `;
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
+});
+
+app.listen(3000);
