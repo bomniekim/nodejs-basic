43fd8c9bae1ef1b26888b4fb913e9f6926630edc
Bomin Kim <bomnie.kim@gmail.com>
2020-05-06 14:28:08 +0900

    조건문을 이용한 홈페이지 구현

--- notfound.js
+++ notfound.js
@@ -6,33 +6,67 @@ var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathName = url.parse(_url, true).pathname;
-  var title = queryData.id;
 
   if (pathName == "/") {
-    fs.readFile(`data/${queryData.id}`, "utf8", function (err, desc) {
-      var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=html">HTML</a></li>
-            <li><a href="/?id=css">CSS</a></li>
-            <li><a href="/?id=javascript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${desc}</p>
-        </body>
-        </html>
-        
-        `;
-      response.writeHead(200);
-      response.end(template);
-    });
+    if (queryData.id === undefined) {
+      // home
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, desc) {
+        var title = "Welcome";
+        var desc = "Hello node.js";
+        var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+              </head>
+              <body>
+              <title>WEB1 - ${title}</title>
+              <h1><a href="/">WEB</a></h1>
+              <ol>
+                <li><a href="/?id=html">HTML</a></li>
+                <li><a href="/?id=css">CSS</a></li>
+                <li><a href="/?id=javascript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2>
+              <p>${desc}</p>
+            </body>
+            </html>
+            
+            `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, desc) {
+        var title = queryData.id;
+        var template = `
+              <!doctype html>
+              <html>
+              <head>
+                <title>WEB1 - ${title}</title>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <title>WEB1 - ${title}</title>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                  <li><a href="/?id=html">HTML</a></li>
+                  <li><a href="/?id=css">CSS</a></li>
+                  <li><a href="/?id=javascript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>${desc}</p>
+              </body>
+              </html>
+              
+              `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
   } else {
     response.writeHead(404);
     response.end("Not found");
